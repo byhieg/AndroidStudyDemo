869f98821bdc4fa80a48ec7096b765601cf643f5
byhieg <byhieg@gmail.com>
2016-10-11 10:10:48 +0800

    修改Activity的代码

--- ActivityDemo/app/src/main/java/com/demo/study/activitydemo/Main2Activity.java
+++ ActivityDemo/app/src/main/java/com/demo/study/activitydemo/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.demo.study.activitydemo;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,15 +11,27 @@ import android.widget.Button;
 public class Main2Activity extends AppCompatActivity {
 
     public Button button;
+
+    public Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         button = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                startActivity(intent);
             }
         });
 
