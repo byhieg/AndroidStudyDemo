2e3f095650a2693d6570e00a1cd45263f5353474
石琦峰 <byhieg@gmail.com>
2017-01-30 20:37:19 +0800

    添加Rxdemo例子——操作符时间戳

--- RxDemo/app/src/main/java/com/demo/study/rxdemo/TimestampActivity.java
+++ RxDemo/app/src/main/java/com/demo/study/rxdemo/TimestampActivity.java
@@ -2,12 +2,48 @@ package com.demo.study.rxdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+import rx.schedulers.Timestamped;
 
 public class TimestampActivity extends AppCompatActivity {
 
+
+    @BindView(R.id.result)
+    public TextView result;
+
+    private Integer[] integers = {1, 3, 5, 2, 24, 7, 5, 86, 23, 43};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timestamp);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.start)
+    public void start(){
+        Observable.from(integers).
+                timestamp(Schedulers.newThread()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(new Action1<Timestamped<Integer>>() {
+                    @Override
+                    public void call(Timestamped<Integer> integerTimestamped) {
+                        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+                        result.append("value: "+integerTimestamped.getValue()+"       time:   ");
+                        result.append(sdf.format(new Date(integerTimestamped.getTimestampMillis()))
+                                +"\n");
+                    }
+                });
     }
 }
