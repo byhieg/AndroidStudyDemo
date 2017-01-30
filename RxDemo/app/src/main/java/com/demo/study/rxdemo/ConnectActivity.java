1063f88f3b2d282e4b38ae5902ba79ebfac14f71
石琦峰 <byhieg@gmail.com>
2017-01-30 19:08:56 +0800

    添加Rxdemo中的rx例子——connect操作符

--- RxDemo/app/src/main/java/com/demo/study/rxdemo/ConnectActivity.java
+++ RxDemo/app/src/main/java/com/demo/study/rxdemo/ConnectActivity.java
@@ -2,12 +2,73 @@ package com.demo.study.rxdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.observables.ConnectableObservable;
 
 public class ConnectActivity extends AppCompatActivity {
+
+    @BindView(R.id.result)
+    public TextView result;
+
+
+    private Integer []integers = {1,2,3,4,5,6,7,8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.start_normal)
+    public void startNormal(){
+        result.setText("");
+        Observable observable = Observable.from(integers);
+        observable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer o) {
+                result.append("观察者A  收到:  "+o+"\n");
+            }
+        });
+
+        observable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer o) {
+                result.append("观察者B  收到:  "+o+"\n");
+
+            }
+        });
+    }
+
+
+    @OnClick(R.id.start_connect)
+    public void startConnect(){
+        result.setText("");
+        ConnectableObservable observable = Observable.from(integers).publish();
+
+        observable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer o) {
+                result.append("观察者A  收到:  "+o+"\n");
+            }
+        });
+
+        observable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer o) {
+                result.append("观察者B  收到:  "+o+"\n");
+
+            }
+        });
+
+        observable.connect();
+    }
+
+
 }
--- RxDemo/app/src/main/java/com/demo/study/rxdemo/TimerActivity.java
+++ RxDemo/app/src/main/java/com/demo/study/rxdemo/TimerActivity.java
@@ -2,12 +2,49 @@ package com.demo.study.rxdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 public class TimerActivity extends AppCompatActivity {
+
+    @BindView(R.id.result)
+    public TextView result;
+
+
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.start)
+    public void start(){
+        subscription = Observable.interval(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers
+                .mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        result.setText(aLong + "");
+
+                    }
+                });
+    }
+
+    @OnClick(R.id.cancel)
+    public void cancel(){
+        subscription.unsubscribe();
     }
 }
