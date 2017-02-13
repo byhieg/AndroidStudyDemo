feaf67c5faa660e88f5dc2250a8a3e576d3c4594
byhieg <byhieg@gmail.com>
2017-02-13 17:14:44 +0800

    修改代码

--- MvpDemo/app/src/main/java/com/byhieg/mvpdemo/UserLoginActivity.java
+++ MvpDemo/app/src/main/java/com/byhieg/mvpdemo/UserLoginActivity.java
@@ -1,13 +1,25 @@
 package com.byhieg.mvpdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    private EditText mEtUsername,mEtPassword;
+    private Button mBtnLogin,mBtnClear;
+    private ProgressBar mPbLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        initViews();
+    }
+
+    private void initViews() {
+        mEtUsername = (EditText)findViewById(R.id.)
     }
 }
