2875885f2e16cd7cd5283e8dc4bf5662b56cdab9
石琦峰 <byhieg@gmail.com>
2017-07-09 22:56:18 +0800

    添加新的Demo

---
+++ AnimationDemo/app/src/main/java/cn/byhieg/animationdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package cn.byhieg.animationdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView);
+        Animation animation = new Rotate3dAnimation(0,180,0,0,100,false);
+        mTextView.startAnimation(animation);
+    }
+}
